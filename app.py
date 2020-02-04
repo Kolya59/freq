4f7552a8859a24a7e7661ee902a4baab20bf66f0
Nikitas Nikolay <kolya.nikitas@yandex.ru>
2020-02-05 00:41:30 +0500

    Initial commit

--- app.py
+++ app.py
@@ -2,21 +2,26 @@ import collections
 import pymorphy2
 import nltk
 
-def save(f:file):
+
+def save(output, tag):
+    output.write('{0} {1}\n'.format(tag[0][0].normal_form, tag[1]))
+
 
 if __name__ == '__main__':
-    f = open('./data/input.txt')
+    f = open('./data/input.txt', 'r')
     # Read text
     source = f.read()
     # Tokenize text
     tokens = nltk.wordpunct_tokenize(source)
     # Calculate frequencies of words
-    counter = collections.Counter(tokens)
-    #ordered = collections.OrderedDict(sorted(counter.items(),key= lambda x: x[1]))
+    ordered = collections.OrderedDict(sorted(collections.Counter(tokens).items(), key=lambda x: x[1], reverse=True))
     # Analyze each word
     morph = pymorphy2.MorphAnalyzer()
-    tags = [morph.parse(i) for i in counter]
+    tags = [(morph.parse(i), ordered[i]) for i in ordered]
 
-    # map(lambda x: , sorted(tags, ))
+    # Save result
+    out = open('./data/output.txt', 'w')
+    for x in tags:
+        save(out, x)
     f.close()
-
+    out.close()
