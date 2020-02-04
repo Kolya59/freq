3d4b9a47c451643f2de96ab40a96c0f6dcc91f12
Nikitas Nikolay <kolya.nikitas@yandex.ru>
2020-02-05 01:03:50 +0500

    Refactored order of sorting

--- app.py
+++ app.py
@@ -4,24 +4,30 @@ import nltk
 
 
 def save(output, tag):
-    output.write('{0} {1}\n'.format(tag[0][0].normal_form, tag[1]))
+    output.write('{0} {1}\n'.format(tag[0], tag[1]))
 
 
 if __name__ == '__main__':
-    f = open('./data/input.txt', 'r')
     # Read text
+    f = open('./data/input.txt', 'r')
     source = f.read()
     # Tokenize text
     tokens = nltk.wordpunct_tokenize(source)
-    # Calculate frequencies of words
-    ordered = collections.OrderedDict(sorted(collections.Counter(tokens).items(), key=lambda x: x[1], reverse=True))
     # Analyze each word
     morph = pymorphy2.MorphAnalyzer()
-    tags = [(morph.parse(i), ordered[i]) for i in ordered]
+    tags = [morph.parse(i) for i in tokens]
+    # Calculate frequencies of words
+    ordered = collections.OrderedDict(
+        sorted(
+            collections.Counter([t[0].normal_form for t in tags]).items(),
+            key=lambda t2: t2[1],
+            reverse=True
+        )
+    )
 
     # Save result
     out = open('./data/output.txt', 'w')
-    for x in tags:
-        save(out, x)
+    for v in ordered:
+        save(out, (v, ordered[v]))
     f.close()
     out.close()
